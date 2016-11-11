ac2c98c552a3485a86fbaa5d6a5788663ecc0e44
saracouto1318 <saracouto1318@hotmail.com>
2016-11-11 17:56:31 +0000

    Menu changed, can do the exit, return and game rules

--- Otrio.pl
+++ Otrio.pl
@@ -498,8 +498,8 @@ dL(_, []):- write(' |').
 %% 		MENUS
 %%-----------------------
 
+exitGame.
 
-%% Perceber como fazer clear screen
 cls :- write('\e[2J').
 
 logo :-         write('|                                                                                 |'), nl,
@@ -515,11 +515,14 @@ logo :-         write('|                                                        
 
 
 mainMenu :-     write(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - '), nl,
+				write('|                                                                                 |'), nl,
 				logo,
                 write('|                                                                                 |'), nl,
+				write('|                                                                                 |'), nl,
+				write('|                                                                                 |'), nl,
                 write('|                                 Welcome!!!	                                  |'), nl,
                 write('|   				                                                  |'), nl,
-				write('|                  Menu:				                          |'), nl,
+				write('|                       				                          |'), nl,
 				write('|   				                                                  |'), nl,
                 write('|                   1. Play                                                       |'), nl,
                 write('|                   2. Game Rules                                                 |'), nl,
@@ -528,6 +531,8 @@ mainMenu :-     write(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
                 write(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - '), nl,
                 nl, write('Option: '), get_char(R), get_char(_), 
                 (R = '1' -> playMenu;
+				 R = '2' -> gameRules;
+				 R = '3' -> exitGame;
 				 mainMenu).
 
 playMenu :-     cls,
@@ -543,5 +548,32 @@ playMenu :-     cls,
                 write('|                                                                                 |'), nl,
                 write(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - '), nl,
                 nl, write('Option: '), get_char(G), get_char(_),
-                (G = '1' -> game_cicle;
+                (G = '1' -> cls, game_cicle;
+				 G = '4' -> cls, mainMenu;
 				 playMenu).
+
+gameRules :-	cls,
+				write(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - '), nl,
+				write('|                                                                                 |'), nl,
+				write('|                                                                                 |'), nl,
+				write('|                  Game Rules:                                                    |'), nl,
+				write('|                                                                                 |'), nl,
+				write('|                                                                                 |'), nl,
+				write('|   -> Each player has 9 pieces, 3 larges, 3 medium and 3 small and each one      |'), nl,
+				write('|      of them must put the respective pieces in empty spaces, not being able     |'), nl,
+				write('|      to move them after their placement                                         |'), nl,
+				write('|                                                                                 |'), nl,
+				write('|   -> If a player can not put one of his pieces, the turm changes to the other   |'), nl,
+				write('|      player, so that they can place pieces until they reach a OTRIO             |'), nl,
+				write('|                                                                                 |'), nl,
+				write('|      How to make a OTRIO:                                                       |'), nl,
+				write('|                                                                                 |'), nl,
+				write('|      1. Have 3 pieces with the same size and color on a board row               |'), nl,
+				write('|      2. Have 3 pieces of increasing or decreasing size on a board row           |'), nl,
+				write('|      3. Have 3 concentic pieces inthe same place                                |'), nl,
+				write('|                                                                                 |'), nl,
+				write('|                                                                                 |'), nl,
+				write('|                     Press enter to return to the main menu                      |'), nl,
+				write('|                                                                                 |'), nl,
+				write(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - '), nl,
+				nl, get_char(_), cls, mainMenu.			
