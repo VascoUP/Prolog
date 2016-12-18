f1728efb937434286a0b07df10edce95df38dec1
VascoUP <vasco_mag_pereira@hotmail.com>
2016-12-18 18:08:52 +0000

    Problem solved

--- Projeto2/Tents.pl
+++ Projeto2/Tents.pl
@@ -13,6 +13,16 @@ trees([(2, 1), (4, 2), (2, 3), (4, 4), (6, 4), (1, 5), (2, 5), (4, 6)]).
 % ======= INITIALIZE BOARD =======
 % ================================
 
+%init_tents(-Board, -Trees, -Vals_Cls, -Vals_Lns)
+init_board(Board, Trees, Vals_Cls, Vals_Lns) :-
+        trees(Trees),
+        vals_cls(Vals_Cls),
+        vals_lns(Vals_Lns),
+        
+        create_board(6, 6, Board),
+        add_trees(Trees, Board),
+        fill_impossible_spaces(Board, Trees).
+        
 % create_board(+NCols, +NLines, -Board)
 create_board(NLines, NCols, Board) :-
         length(Board2, NLines),
@@ -25,7 +35,7 @@ create_lines(NCols, [_|R], [N|T]) :-
         !, create_lines(NCols, R, T).
 
 
-% add_trees(+TreeCoords, +Board, -BoardRes)
+% add_trees(+TreeCoords, +Board)
 add_trees([], _).
 add_trees([(X, Y)|Trees], Board) :-
         value_position(X, Y, Board, 0), !,
@@ -117,39 +127,19 @@ value_down(X, Y, Board, Values, NValues) :-
         append([Value], Values, NValues), !.
 value_down(_, _, _, Values, Values).
 
-
-% value_right_top(-X, -Y, -Board, +Value)
-value_right_top(X, Y, Board, Values, NValues) :-
-        X1 is X + 1,
-        Y1 is Y - 1,
-        value_position(X1, Y1, Board, Value),
-        append([Value], Values, NValues), !.
-value_right_top(_, _, _, Values, Values).
-
-% value_left_top(-X, -Y, -Board, +Value)
-value_left_top(X, Y, Board, Values, NValues) :-
-        X1 is X - 1,
-        Y1 is Y - 1,
-        value_position(X1, Y1, Board, Value),
-        append([Value], Values, NValues), !.
-value_left_top(_, _, _, Values, Values).
-
-% value_right_down(-X, -Y, -Board, -Value, +NValues)
-value_right_down(X, Y, Board, Values, NValues) :-
-        X1 is X + 1,
+% value_bottom_right(-X, -Y, -Board, +Value)
+value_bottom_right(X, Y, Board, Values, NValues) :-
         Y1 is Y + 1,
+        X1 is X + 1,
         value_position(X1, Y1, Board, Value),
         append([Value], Values, NValues), !.
-value_right_down(_, _, _, Values, Values).
+value_bottom_right(_, _, _, Values, Values).
 
-% value_left_down(-X, -Y, -Board, +Value)
-value_left_down(X, Y, Board, Values, NValues) :-
-        X1 is X - 1,
-        Y1 is Y + 1,
-        value_position(X1, Y1, Board, Value),
+% value_bottom_left(-X, -Y, -Board, +Value)
+value_pos(X, Y, Board, Values, NValues) :-
+        value_position(X, Y, Board, Value),
         append([Value], Values, NValues), !.
-value_left_down(_, _, _, Values, Values).
-
+value_pos(_, _, _, Values, Values).
 
 % adjancent_values(-X, -Y, -Board, +Values) 
 adjancent_values(X, Y, Board, Values) :-
@@ -158,13 +148,18 @@ adjancent_values(X, Y, Board, Values) :-
         value_top(X, Y, Board, NValues2, NValues3),
         value_down(X, Y, Board, NValues3, Values), !.
 
-% all_values_around(+X, +Y, +Board, -Values) 
-all_values_around(X, Y, Board, Values) :-
-        adjancent_values(X, Y, Board, NValues),
-        value_right_top(X, Y, Board, NValues, NValues1),
-        value_left_top(X, Y, Board, NValues1, NValues2),
-        value_right_down(X, Y, Board, NValues2, NValues3),
-        value_left_down(X, Y, Board, NValues3, Values), !.
+% square_value(+X, +Y, +Board, -Values)
+square_value(X, Y, Board, Values) :-
+        value_pos(X, Y, Board, [], NValues),
+        value_right(X, Y, Board, NValues, NValues2),
+        value_down(X, Y, Board, NValues2, NValues3),
+        value_bottom_right(X, Y, Board, NValues3, Values), !.
+
+% get_arr_board(+Board, -Res)
+get_arr_board([], []).
+get_arr_board([H|T], Res) :-
+        get_arr_board(T, Res2),
+        append(H, Res2, Res). 
 
 
 
@@ -172,7 +167,7 @@ all_values_around(X, Y, Board, Values) :-
 % ========= RESTRICTIONS =========
 % ================================
 
-% sum_trees( +Trees, +Board )
+% sum_trees(+Trees, +Board)
 sum_trees([], _):- !.
 sum_trees( [(X, Y) | Trees], Board ) :-
         adjancent_values(X, Y, Board, Values),
@@ -180,78 +175,57 @@ sum_trees( [(X, Y) | Trees], Board ) :-
         !, sum_trees( Trees, Board ).
 
 
-% sum_line( +Board, +Value, +Line )
+% sum_line(+Board, +Value, +Line)
 sum_line( Board, Value, Line ) :-
         nth1( Line, Board, H ),
         sum(H, #=, Value).
 
-% sum_lines( +Board, +Vals_Line )
+% sum_lines(+Board, +Vals_Line)
 sum_lines( _, [] ).
 sum_lines( Board, [(Line, Value) | T] ) :-
        sum_line( Board, Value, Line ),
        !, sum_lines( Board, T ).
 
 
-% sum_col( +Board, +Value, +Column )
+% sum_col(+Board, +Value, +Column)
 sum_col( Board, Value, Column ) :-
-        findall(Col, (
-                        length(Board, Len), domain([NumLine], 1, Len), 
-                        nth1(NumLine, Board, Line), nth1(Column, Line, Col)), 
-                Vals),
+        transpose(Board, Res),
+        nth1(Column, Res, Vals),
         sum(Vals, #=, Value).
 
-% sum_cols( +Board, +Vals_Cols )
+% sum_cols(+Board, +Vals_Cols)
 sum_cols( _, [] ).
 sum_cols( Board, [(Column, Value) | T] ) :-
        sum_col( Board, Value, Column ),
        !, sum_cols( Board, T ).
 
 
-% get_square( +Board, +X, +Y, -Square )
-get_square( Board, X, Y, Square ) :-
-        length( Board, LenY ),
-        nth1( 1, Board, L ),
-        length( L, LenX ), !,
-        
-        LenY >= Y + 1,
-        LenX >= X + 1,
-        
-        % X and Y
-        nth1( Y, Board, H1 ),
-        nth1( X, H1, V1 ),
-        
-        % X+1 and Y
-        X1 is X + 1,
-        nth1( X1, H1, V2 ),
-        
-        % X and Y+1
-        Y1 is Y + 1,
-        nth1( Y1, Board, H2 ),
-        nth1( X, H2, V3 ),
-        
-        % X+1 and Y+1
-        nth1( X1, H2, V4 ), !,
-        
-        append( [V1, V2, V3, V4], [], Square ), write(Square).
-
-
-% sum_squares_cols( +Board, +X, +Y )
-sum_squares_cols( Board, X, Y ) :-
-        get_square( Board, X, Y, Square ), !,
-        X1 is X + 1,
-        sum(Square, #<, 2), !,
-        sum_squares_cols( Board, X1, Y ).
-
-% sum_squares( +Board, +X, +Y )
-sum_squares( Board, X, Y ) :-
-        sum_squares_cols( Board, X, Y ), !,
+% sum_squares_cols(+Board, +X, +Y)
+sum_square(Board, X, Y) :-
+        nth1(1, Board, N),
+        length(N, NCols),
+        X >= NCols,
         Y1 is Y + 1, !,
-        sum_squares( Board, X, Y1 ).
-sum_squares( _, _, _ ).
+        sum_square(Board, 1, Y1).
+sum_square(Board, _, Y) :-
+        length(Board, NLines),
+        Y >= NLines, !.
+sum_square(Board, X, Y) :-
+        square_value(X, Y, Board, Square),
+        sum(Square, #<, 2), 
+        X1 is X + 1, !,
+        sum_square(Board, X1, Y).
 
-% sum_squares( +Board )
-sum_squares( Board ) :-
-        !, sum_squares( Board, 1, 1 ).
+% sum_squares(+Board)
+sum_square( Board ) :-
+        !, sum_square( Board, 1, 1 ).
+   
+% sum_board(+Board, +TreeCoords)                    
+sum_board(Board, TreeCoords) :-
+        get_arr_board(Board, Res),
+        length(TreeCoords, Total),
+        sum(Res, #=, Total).
+        
 
 
 % domain_board( +Board, +Min, +Max )
@@ -274,24 +248,18 @@ labeling_board([H|T]) :-
 
 % tents
 tents:-
-        trees(Trees),
-        vals_cls(Vals_Cols),
-        vals_lns(Vals_Lines), !,
-        
-        create_board(6, 6, Board),
-        add_trees(Trees, Board),
-        fill_impossible_spaces(Board, Trees),
+        init_board(Board, Trees, Vals_Cols, Vals_Lines), !,
         
         display_board(Board, Trees),
         
         domain_board(Board, 0, 1),
         
+        sum_board(Board, Trees),
         sum_trees(Trees, Board),
         sum_lines(Board, Vals_Lines),
         sum_cols(Board, Vals_Cols),
-        sum_squares(Board),
-        write("Oi"), nl,
+        sum_square(Board),
         
         labeling_board(Board),
-        
+
         display_board(Board, Trees).
